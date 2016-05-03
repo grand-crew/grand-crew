5970f5604c5f5bb4434ffa26bd8aaa494c72c5db
Gamble McAdam <gamble.mcadam@gmail.com>
2016-05-03 17:40:23 -0500

    test

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -3,7 +3,6 @@ import { Container } from 'react-responsive-grid'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Headroom from 'react-headroom'
-import Griddle from 'griddle-react'
 
 import { rhythm } from 'utils/typography'
 
@@ -14,19 +13,44 @@ module.exports = React.createClass({
     }
   },
   render () {
-    const data = [
-      {
-        January: 35,
-        February: 20,
-        March: 27,
-        April: 32,
-        May: 23,
-        June: 42,
-      },
-    ]
-
     return (
-      <Griddle results={data} />
+      <div>
+        <Headroom
+          wrapperStyle={{
+            marginBottom: rhythm(1),
+          }}
+          style={{
+            background: 'lightgray',
+          }}
+        >
+          <Container
+            style={{
+              maxWidth: 960,
+              paddingTop: 0,
+              padding: `${rhythm(1)} ${rhythm(1/2)}`,
+            }}
+          >
+            <Link
+              to={prefixLink('/')}
+              style={{
+                color: 'black',
+                textDecoration: 'none',
+              }}
+            >
+              Gatsby!!!
+            </Link>
+          </Container>
+        </Headroom>
+        <Container
+          style={{
+            maxWidth: 960,
+            padding: `${rhythm(1)} ${rhythm(1/2)}`,
+            paddingTop: 0,
+          }}
+        >
+          {this.props.children}
+        </Container>
+      </div>
     )
   },
 })
